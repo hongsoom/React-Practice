ca9581c54223faf5309ff6452c4b4c33eae1e41e
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-15 22:12:29 +0900

    props and defaultProps and children

--- src/03/Component/App.js
+++ src/03/Component/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent name="React">리액트</MyComponent>; // MyComponent의 props 값을 지정, 컴포넌트 태그 사이 문자열(리액트)을 MyComponent 내부에 보여줌
 };
 
 export default App;
--- src/03/Component/MyComponent.js
+++ src/03/Component/MyComponent.js
@@ -1,5 +1,15 @@
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = (props) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {props.name} 입니다. <br />
+      children 값은 {props.children} 입니다.
+    </div> // name이라는 props를 렌더링하도록 설정하고 태그 사이에 내용을 보여주는 children
+  );
+};
+
+MyComponent.defaultProps = {
+  // props 기본값 설정
+  name: "기본 이름",
 };
 
 // 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정
