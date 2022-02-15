aa28bbb83a1c4cdbbf151ef4d180c872ef5a5082
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-15 23:37:54 +0900

    class component

--- src/03/Component/MyComponent.js
+++ src/03/Component/MyComponent.js
@@ -1,15 +1,20 @@
+import { Component } from "react";
 import PropTypes from "prop-types"; // PropTypes를 사용하기 위해 불러옴
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  // 비구조화 할당 (객체에서 값을 추출하는)
-  return (
-    <div>
-      안녕하세요, 제 이름은 {name} 입니다. <br />
-      children 값은 {children} 입니다. <br />
-      제가 좋아하는 숫자는 {favoriteNumber} 입니다.
-    </div> // name,favoriteNumber이라는 props를 렌더링하도록 설정하고 태그 사이에 내용을 보여주는 children
-  );
-};
+class MyComponent extends Component {
+  // class형 컴포넌트
+  render() {
+    // class형 컴포넌트에서 props를 사용할 때 render함수를 꼭 사용
+    const { name, favoriteNumber, children } = this.props; // 비구조화 할당 (객체에서 값을 추출하는)
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name} 입니다. <br />
+        children 값은 {children} 입니다. <br />
+        제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+      </div> // name,favoriteNumber이라는 props를 렌더링하도록 설정하고 태그 사이에 내용을 보여주는 children
+    );
+  }
+}
 
 MyComponent.defaultProps = {
   // props 기본값 설정
