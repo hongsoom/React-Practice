a7e3daabfde80bc00a8fa0639b8fb2de0c2bba9d
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-15 23:31:46 +0900

    destructuring assignment and propTypes and isRequired

--- src/03/Component/App.js
+++ src/03/Component/App.js
@@ -1,7 +1,11 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>; // MyComponent의 props 값을 지정, 컴포넌트 태그 사이 문자열(리액트)을 MyComponent 내부에 보여줌
+  return (
+    <MyComponent name="React" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  ); // MyComponent name,favoriteNumbe의 props 값을 지정,  컴포넌트 태그 사이 문자열(리액트)을 MyComponent 내부에 보여줌
 };
 
 export default App;
--- src/03/Component/MyComponent.js
+++ src/03/Component/MyComponent.js
@@ -1,15 +1,25 @@
-const MyComponent = (props) => {
+import PropTypes from "prop-types"; // PropTypes를 사용하기 위해 불러옴
+
+const MyComponent = ({ name, favoriteNumber, children }) => {
+  // 비구조화 할당 (객체에서 값을 추출하는)
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name} 입니다. <br />
-      children 값은 {props.children} 입니다.
-    </div> // name이라는 props를 렌더링하도록 설정하고 태그 사이에 내용을 보여주는 children
+      안녕하세요, 제 이름은 {name} 입니다. <br />
+      children 값은 {children} 입니다. <br />
+      제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+    </div> // name,favoriteNumber이라는 props를 렌더링하도록 설정하고 태그 사이에 내용을 보여주는 children
   );
 };
 
 MyComponent.defaultProps = {
   // props 기본값 설정
   name: "기본 이름",
+};
+
+MyComponent.propTypes = {
+  // 필수 props를 지정하거나 props의 타입을 지정할때 사용
+  name: PropTypes.string, // props의 타입 string
+  favoriteNumber: PropTypes.number.isRequired, // propTypes를 지정하지 않았을 때, 필수 props의 타입이 다른 경우 경고 메시지를 띄워 줌
 };
 
 // 다른 파일에서 이 파일을 import할 때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정
