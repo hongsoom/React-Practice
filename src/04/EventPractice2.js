32f8fa251d8c2bc74b50a907f85fd087e8ce65c4
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-17 23:27:49 +0900

    multiple inputs

--- src/04/EventPractice2.js
+++ src/04/EventPractice2.js
@@ -2,22 +2,37 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "", // 초깃값 설정
   };
 
   handleChange = (e) => {
-    this.setState({ message: e.target.value });
+    // 객체 안에서 Key를 []로 감싸면 실제 값이 key 값으로 사용된다.
+    // 예를 들면 const name = 'key';
+    //          const obj = { [name] : 'value' };
+    //     결과 : 'key' : 'value'
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleClick = () => {
-    alert(this.state.message);
-    this.setState({ message: "" });
+    alert(this.state.username + ":" + this.state.message);
+    this.setState({
+      username: "",
+      message: "",
+    });
   };
 
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
         <input
           type="text"
           name="message"
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Counter from "./03/Component/Counter";
 import Say from "./03/Component/say";
 import EventPractice from "./04/EventPractice";
+import EventPractice2 from "./04/EventPractice2";
 
 const App = () => {
   // return <Counter />;
   // return <Say />;
-  return <EventPractice />;
+  //return <EventPractice />;
+  return <EventPractice2 />;
 };
 
 export default App;
