94c20fd7dd7eb674f8c4353dc8d050ff63cfec2b
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-17 23:41:14 +0900

    form 객체

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Counter from "./03/Component/Counter";
 import Say from "./03/Component/say";
 import EventPractice from "./04/EventPractice";
 import EventPractice2 from "./04/EventPractice2";
+import FuctionEventPractice from "./04/Function_EventPractice";
 
 const App = () => {
   // return <Counter />;
   // return <Say />;
   //return <EventPractice />;
-  return <EventPractice2 />;
+  //return <EventPractice2 />;
+  return <FuctionEventPractice />;
 };
 
 export default App;
