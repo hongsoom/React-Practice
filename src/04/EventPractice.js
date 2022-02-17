c43fcdbae2c1ddb2e202536a82bb484a94d68587
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-17 21:52:19 +0900

    class state

--- src/04/EventPractice.js
+++ src/04/EventPractice.js
@@ -1,6 +1,10 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "", // 초깃값 설정
+  };
+
   render() {
     return (
       <div>
@@ -9,11 +13,13 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
           onChange={(e) => {
-            console.log(e.target.value); // 값이 바뀔때마다 바뀌는 값(e.target.value)을 콘솔에 기록
-            // e.target.value 값을 통해 이벤트 객체에 담겨있는 현재의 텍스트 값을 읽어올 수 있음.
+            this.setState({
+              message: e.target.value, // setState를 이용하여 state 값 업데이트
+            });
           }}
-        ></input>
+        />
       </div>
     );
   }
