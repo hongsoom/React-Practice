66fd3a4f13221e587c49cbfdb9889318b2afa7b9
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-02-23 22:01:00 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -5,46 +5,55 @@ import Say from "./03/Component/say";
 import EventPractice from "./04/EventPractice";
 import EventPractice2 from "./04/EventPractice2";
 import FuctionEventPractice from "./04/Function_EventPractice";
-import LifeCycleSample from "./07/lLifeCycle/LifeCycleSample";
-import ErrorBoundary from "./07/lLifeCycle/ErrorBoundary";
+import LifeCycleSample from "./07/LifeCycle/LifeCycleSample";
+import ErrorBoundary from "./07/LifeCycle/ErrorBoundary";
+import { Route } from "react-router-dom";
+import About from "./13/ReactRouter/About";
+import Home from "./13/ReactRouter/Home";
 
-//const App = () => {
-// return <Counter />;
-// return <Say />;
-//return <EventPractice />;
-//return <EventPractice2 />;
-// return <FuctionEventPractice />;
+const App = () => {
+  // return <Counter />;
+  // return <Say />;
+  //return <EventPractice />;
+  //return <EventPractice2 />;
+  // return <FuctionEventPractice />;
+  return (
+    <div>
+      <Route path="/" Component={Home} exact={true} />
+      <Route path="/about" Component={About} />
+    </div>
+  );
+};
+
+//function getRandomColor() {
+// state의 color 값을 랜덤 색상으로 설정
+// return "#" + Math.floor(Math.random() * 16777215).toString(16);
+//}
+
+//class App extends Component {
+//state = {
+// color: "#000000",
 //};
 
-function getRandomColor() {
-  // state의 color 값을 랜덤 색상으로 설정
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
+//handleClick = () => {
+//  this.setState({
+//    color: getRandomColor(),
+//  });
+//};
 
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    // 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
-    // LifeCycleSample 컴포넌트에 color 값을 props로 설정
-    // ErrorBoundarY가 LifeCycleSample를 감싸줌
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+//render() {
+// 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
+// LifeCycleSample 컴포넌트에 color 값을 props로 설정
+// ErrorBoundarY가 LifeCycleSample를 감싸줌
+// return (
+//  <div>
+//   <button onClick={this.handleClick}>랜덤 색상</button>
+//   <ErrorBoundary>
+//    <LifeCycleSample color={this.state.color} />
+//   </ErrorBoundary>
+// </div>
+// );
+// }
+//}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  // BrowserRouter 컴포넌트는 웹 애플리케이션에 HTML5의 History API를 사용하여
+  // 페이지를 새로고침하지 않고도 주소를 변경하고, 현재 주소에 관련된 정보를 props로 쉽게 조회하거나 사용할 수 있도록 해줌
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  //</React.StrictMode>,
   document.getElementById("root")
 );
 
