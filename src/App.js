7b165a37ddee7f15c8e5066f47bd37c724aa979b
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-03-15 18:08:25 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -13,9 +13,13 @@ import Home from "./13/ReactRouter/Home";
 import Profile from "./13/ReactRouter/Profile";
 import Profiles from "./13/ReactRouter/Profiles";
 import HistorySample from "./13/ReactRouter/HistorySample";
+import Counter1 from "./08/Hooks/Counter";
 
 const App = () => {
   return (
+    <Counter1 />
+
+    /*
     // exact가 true이면 지정된 경로와 정확히 일치하는 경우에만 컴포넌트를 보여줌
     // username이라는 동적 라우팅값을 주었고 username이라는 parameter를 Profile로 보냄
     <div>
@@ -43,36 +47,37 @@ const App = () => {
     </div>
   );
 };
+*/
+    //function getRandomColor() {
+    // state의 color 값을 랜덤 색상으로 설정
+    // return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    //}
 
-//function getRandomColor() {
-// state의 color 값을 랜덤 색상으로 설정
-// return "#" + Math.floor(Math.random() * 16777215).toString(16);
-//}
+    //class App extends Component {
+    //state = {
+    // color: "#000000",
+    //};
 
-//class App extends Component {
-//state = {
-// color: "#000000",
-//};
+    //handleClick = () => {
+    //  this.setState({
+    //    color: getRandomColor(),
+    //  });
+    //};
 
-//handleClick = () => {
-//  this.setState({
-//    color: getRandomColor(),
-//  });
-//};
-
-//render() {
-// 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
-// LifeCycleSample 컴포넌트에 color 값을 props로 설정
-// ErrorBoundarY가 LifeCycleSample를 감싸줌
-// return (
-//  <div>
-//   <button onClick={this.handleClick}>랜덤 색상</button>
-//   <ErrorBoundary>
-//    <LifeCycleSample color={this.state.color} />
-//   </ErrorBoundary>
-// </div>
-// );
-// }
-//}
-
+    //render() {
+    // 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
+    // LifeCycleSample 컴포넌트에 color 값을 props로 설정
+    // ErrorBoundarY가 LifeCycleSample를 감싸줌
+    // return (
+    //  <div>
+    //   <button onClick={this.handleClick}>랜덤 색상</button>
+    //   <ErrorBoundary>
+    //    <LifeCycleSample color={this.state.color} />
+    //   </ErrorBoundary>
+    // </div>
+    // );
+    // }
+    //}
+  );
+};
 export default App;
