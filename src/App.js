c38360779517140bb274fb3b2aece5173234f450
hongsoom <99132215+hongsoom@users.noreply.github.com>
2022-03-15 21:44:02 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -16,9 +16,12 @@ import HistorySample from "./13/ReactRouter/HistorySample";
 import Counter1 from "./08/Hooks/Counter";
 import { useState } from "react";
 import Info from "./08/Hooks/Info";
+import ReducerCounter from "./08/Hooks/ReducerCounter";
 
 const App = () => {
-  const [visible, setVisible] = useState(false); // 초기값 false
+  return <ReducerCounter />;
+
+  /*const [visible, setVisible] = useState(false); // 초기값 false
   return (
     <div>
       <button
@@ -27,11 +30,12 @@ const App = () => {
         }}
       >
         {visible ? "숨기기" : "보이기"}{" "}
-        {/* Visible이 true일때 숨기기 버튼 보임, false일때 보이기 버튼 보임*/}
+        {/* Visible이 true일때 숨기기 버튼 보임, false일때 보이기 버튼 보임/}
       </button>
       <hr />
-      {visible && <Info />} {/* Visible이 true일때 Info component 보임*/}
-    </div>
+      {visible && <Info />} {/* Visible이 true일때 Info component 보임/}
+    </div> 
+    
     //<Counter1 />
 
     /*
@@ -63,36 +67,35 @@ const App = () => {
   );
 };
 */
-    //function getRandomColor() {
-    // state의 color 값을 랜덤 색상으로 설정
-    // return "#" + Math.floor(Math.random() * 16777215).toString(16);
-    //}
+  //function getRandomColor() {
+  // state의 color 값을 랜덤 색상으로 설정
+  // return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  //}
 
-    //class App extends Component {
-    //state = {
-    // color: "#000000",
-    //};
+  //class App extends Component {
+  //state = {
+  // color: "#000000",
+  //};
 
-    //handleClick = () => {
-    //  this.setState({
-    //    color: getRandomColor(),
-    //  });
-    //};
+  //handleClick = () => {
+  //  this.setState({
+  //    color: getRandomColor(),
+  //  });
+  //};
 
-    //render() {
-    // 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
-    // LifeCycleSample 컴포넌트에 color 값을 props로 설정
-    // ErrorBoundarY가 LifeCycleSample를 감싸줌
-    // return (
-    //  <div>
-    //   <button onClick={this.handleClick}>랜덤 색상</button>
-    //   <ErrorBoundary>
-    //    <LifeCycleSample color={this.state.color} />
-    //   </ErrorBoundary>
-    // </div>
-    // );
-    // }
-    //}
-  );
+  //render() {
+  // 버튼을 렌더링하고, 누를 때마다 handleClick 메소드 호출
+  // LifeCycleSample 컴포넌트에 color 값을 props로 설정
+  // ErrorBoundarY가 LifeCycleSample를 감싸줌
+  // return (
+  //  <div>
+  //   <button onClick={this.handleClick}>랜덤 색상</button>
+  //   <ErrorBoundary>
+  //    <LifeCycleSample color={this.state.color} />
+  //   </ErrorBoundary>
+  // </div>
+  // );
+  // }
+  //}
 };
 export default App;
